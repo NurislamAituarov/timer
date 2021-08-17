9b1bf0757cf2c59fc57c70423c6f2dc7adb979d2
nurislam <nurislam-1994@mail.ru>
2021-08-17 12:44:43 +0600

    timer

---
+++ timer.js
@@ -0,0 +1,75 @@
+const deadline = '2021-07-05';
+
+function getTimeRemaining(endtime){
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor( (t/(1000*60*60*24) ) );
+    const hours = Math.floor( (t/(1000*60*60) % 24) );
+    const minutes = Math.floor( (t/1000/60) % 60);
+    const seconds = Math.floor( (t/1000) % 60);
+    return {
+        'total':t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+    };
+}
+function getZero(num){
+    if(num>=0 && num < 10){
+        return '0' + num;
+    }else{
+        return num
+    }
+}
+ 
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days =timer.querySelector('.days');
+    const hours =timer.querySelector('.hours');
+    const minutes =timer.querySelector('.minutes');
+    const seconds =timer.querySelector('.seconds');
+    const timeInterval = setInterval(updateClock,1000)
+
+    updateClock()
+    
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if(t.total <= 0){
+            clearInterval(timeInterval)
+        }
+    }
+}
+setClock('.span',deadline)
+
+
+
+/////////МАШИНКА
+
+
+const btn = document.querySelector('button');
+const car = document.querySelector('img')
+const stop = document.querySelector('.stop')
+let index = 0;
+function drive(){
+    if(index>1268){
+        index = 0;
+        index++;
+        car.style.left = index + 'px';
+    }else{
+        index++;
+        // car.style.top = index + 'px';
+        car.style.left = index + 'px';
+    }
+}
+btn.addEventListener('click',function(){
+    const myDrive = setInterval(drive,20);
+    stop.addEventListener('click',function(){
+        clearInterval(myDrive)
+    })
+})
+
